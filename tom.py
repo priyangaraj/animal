8176cd2e907a1ad86580252a5814249607841354
priyangaraj <priyaraj0406@gmail.com>
2016-06-07 12:10:59 +0530

    Create tom.py

---
+++ tom.py
@@ -0,0 +1,18 @@
+def str_to_int(s):
+    ctr = i = 0
+    for c in reversed(s):
+        i += (ord(c) - 48) * (10 ** ctr)
+        ctr += 1
+    return i
+
+print
+for s in ('0', '1', '2', '3', '12', '123', '234', '456', '567'):
+    i = str_to_int(s)
+    print(format(s, i))
+
+print
+print
+for i in range(50):
+    s = str(i)
+    j = str_to_int(s)
+    print(format(s, j))
